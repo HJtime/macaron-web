8d49189f38fe43f4713d8bbab1a36aa80a1fdc40
HJtime <72139168+HJtime@users.noreply.github.com>
2021-01-12 21:53:31 +0900

    JS new slide

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function showSlides(){
 
     slides[slideIndex-1].style.display = 'block';
 
-    myVar = setTimeout(showSlides, 3000);
+    myVar = setTimeout(showSlides, 2500);
 }//showSlides
 
 function stopSlide(){
@@ -53,4 +53,53 @@ function currentSlide(num){
     
     slides[slideIndex].style.display = 'block';
 }
+
+// 신메뉴 슬라이드
+const newWrapper=document.querySelector('.new__wrapper');
+const newSlides=document.querySelectorAll('.new__slides');
+const newSlidez=document.querySelector('.new__slides');
+const newSlide=document.querySelector('.new__slide');
+const newtotal=newSlides.length;
+
+let newWidth=newSlidez.clientWidth;
+let newSlideIndex=0;
+
+
+function slidePosition(){
+    for(let i=0; i<newSlides.length; i++){
+        newSlides[i].style.left = `${(newWidth * i)}px`;
+    }
+}
+
+slidePosition();
+
+newSlide.style.width=newWidth*newtotal+'px';
+
+function newShowSlides(n) {
+    newSlideIndex = n;
+    if (newSlideIndex == -1) {
+        newSlideIndex = newtotal - 1;
+    } else if (newSlideIndex === newtotal) {
+        newSlideIndex = 0;
+    }
+    newSlide.style.left = -(newWidth * newSlideIndex) + 'px';
+}
+
+function plusSlides(n){
+    newShowSlides(newSlideIndex += n);
+}
+
+function currentSlide(n) {
+    newShowSlides(newSlideIndex = n);
+}
+
+const nextBtn = document.querySelector('.new__controls .next');
+const prevBtn = document.querySelector('.new__controls .prev');
+
+nextBtn.addEventListener('click', function () {
+    plusSlides(1);
+});
+prevBtn.addEventListener('click', function () {
+    plusSlides(-1);
+});
 
