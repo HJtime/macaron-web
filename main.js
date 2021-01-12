15f49dddfcfdfe4796884ac9ffb40884c1b145dd
HJtime <72139168+HJtime@users.noreply.github.com>
2021-01-12 21:41:31 +0900

    JS main slide

--- main.js
+++ main.js
@@ -13,3 +13,44 @@ window.addEventListener('scroll', () =>{
         navbar.classList.remove('navbar__white');
     }
 });
+
+// 메인 슬라이드
+const slides = document.querySelectorAll('.main__slides');
+let slideIndex = 0;
+
+let myVar;
+
+showSlides();
+
+function showSlides(){
+    for(let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    
+    slideIndex++;
+
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+
+    slides[slideIndex-1].style.display = 'block';
+
+    myVar = setTimeout(showSlides, 3000);
+}//showSlides
+
+function stopSlide(){
+    clearTimeout(myVar);
+} //stopSlide
+
+/* ========= */
+function currentSlide(num){
+    stopSlide();
+    slideIndex = num;
+
+    for(let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    
+    slides[slideIndex].style.display = 'block';
+}
+
